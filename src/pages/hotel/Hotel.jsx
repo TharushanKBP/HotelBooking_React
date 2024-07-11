e0ad8e3c4ddcff7b617ff98419529ae03d6806e7
TharushanKBP <129367666+TharushanKBP@users.noreply.github.com>
2024-07-11 11:55:47 +0530

    update hotel part

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,10 +1,79 @@
-import React from 'react'
+import "./hotel.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
+import Footer from "../../components/footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const Hotel = () => {
-    return(
-     <div>Hotel</div>
-    )
-    
-}
+  const photos = [
+    {
+      src: "https://upload.wikimedia.org/wikipedia/commons/1/1a/24701-nature-natural-beauty.jpg",
+    },
+    {
+      src: "https://upload.wikimedia.org/wikipedia/commons/1/1a/24701-nature-natural-beauty.jpg",
+    },
+    {
+      src: "https://upload.wikimedia.org/wikipedia/commons/1/1a/24701-nature-natural-beauty.jpg",
+    },
+    {
+      src: "https://upload.wikimedia.org/wikipedia/commons/1/1a/24701-nature-natural-beauty.jpg",
+    },
+  ];
 
-export default Hotel
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <button className="bookNow">Reserve or Book Now!</button>
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Eltion St 125 New York</span>
+          </div>
+          <span className="hotelDistance">
+            Excellent location - 500m from center
+          </span>
+          <span className="hotelPriceHighlight">
+            Book a stay over $114 at this property and get a free airport taxi
+          </span>
+          <div className="hotelImages">
+            {photos.map((photo, index) => (
+              <div className="hotelImgWrapper" key={index}>
+                <img src={photo.src} alt="Hotel view" className="hotelImg" />
+              </div>
+            ))}
+          </div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Stay in the heart of Krakow</h1>
+              <p className="hotelDesc">
+                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
+                Street Apartments has accommodations with air conditioning and
+                free WiFi.
+              </p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Perfect for a 9-night stay!</h1>
+              <span>
+                Located in the real heart of Krakow, this property has an
+                excellent location score of 9.8!
+              </span>
+              <h2>
+                <b>$945</b> (9 nights)
+              </h2>
+              <button>Reserve or Book Now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <MailList/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Hotel;
